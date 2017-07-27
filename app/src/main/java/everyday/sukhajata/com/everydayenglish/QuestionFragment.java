71966bbdef753d695f82c67548ba392501c1135d
Administrator <sukhajata@gmail.com>
2017-07-27 10:37:26 +0700

    question fragment

---
+++ app/src/main/java/everyday/sukhajata/com/everydayenglish/QuestionFragment.java
@@ -0,0 +1,96 @@
+package everyday.sukhajata.com.everydayenglish;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import everyday.sukhajata.com.everydayenglish.interfaces.SlideCompletedListener;
+import everyday.sukhajata.com.everydayenglish.model.Slide;
+import everyday.sukhajata.com.everydayenglish.model.SlideMedia;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SlideCompletedListener} interface
+ * to handle interaction events.
+ * Use the {@link QuestionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class QuestionFragment extends Fragment {
+
+    private Slide mPage;
+    private String mImageUrl;
+    private FrameLayout mSelectedImageFrame;
+    private int errorCount;
+
+    private SlideCompletedListener mListener;
+
+
+    public QuestionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param slide Parameter 1.
+     * @param imageUrl Parameter 2.
+     * @return A new instance of fragment QuestionFragment.
+     */
+    public static QuestionFragment newInstance(Slide slide, String imageUrl) {
+        QuestionFragment fragment = new QuestionFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(LessonActivity.ARG_NAME_SLIDE, slide);
+        args.putString(LessonActivity.ARG_NAME_IMAGE_URL, imageUrl);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mPage = getArguments().getParcelable(LessonActivity.ARG_NAME_SLIDE);
+            mImageUrl = getArguments().getString(LessonActivity.ARG_NAME_IMAGE_URL);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_question, container, false);
+
+
+
+        return view;
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof SlideCompletedListener) {
+            mListener = (SlideCompletedListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+}
